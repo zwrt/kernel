77ba136829d0ce0fa6683e80d93fc72727ef2c02
zwrt <155215126+zwrt@users.noreply.github.com>
2024-01-27 17:54:14 +0800

    Update main.sh

--- .github/main.sh
+++ .github/main.sh
@@ -23,5 +23,5 @@ rm -rf $1
 wget -O ./hosts https://raw.githubusercontent.com/skywrt/hosts/main/hosts
 git clone https://github.com/ophub/amlogic-s9xxx-armbian && mvdir amlogic-s9xxx-armbian
 wget -O ./action.yml https://raw.githubusercontent.com/ophub/amlogic-s9xxx-armbian/main/action.yml
-sed -i 's/default: "ophub/kernel"/default: "zwrt/kernel"/g' ./action.yml
+sed -i 's/default: "ophub/default: "zwrt/g' ./action.yml
 sed -i 's/default: "-ophub"/default: ""/g' ./action.yml
